c1cc93b5c0999d36676c482402af9e376feebcdf
Kirichenko Nikolay <nikolayk@corra.com>
2019-12-16 09:42:12 +0200

    Add base map

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import GoogleMaps from "./GoogleMaps";
+
+function App() {
+  return (
+    <div className="App">
+        <div className={'content'}>
+          <GoogleMaps/>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+export default App;
---
+++ src/GoogleMaps.js
@@ -0,0 +1,49 @@
+import React, {Component, useContext, useEffect} from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+export class MapContainer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedPlace: {
+                name: 'some name'
+            }
+        }
+    }
+
+    onInfoWindowClose (){
+
+    }
+
+    onMarkerClick (){
+
+    }
+
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={50}
+                initialCenter={{
+                    lat: 49.853191,
+                    lng: 36.283477
+                }}
+            >
+
+                <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
+            </Map>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyAeTXiA9DcOqTfEbtTubzjMn5lChDuKjFU')
+})(MapContainer)
